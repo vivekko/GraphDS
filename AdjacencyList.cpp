4e9ba563d1a222562d9bc1754e72111daedbf065
Vivekko <vivekojha961@gmail.com>
2021-04-28 18:59:28 +0530

    Storing Graphs

---
+++ AdjacencyList.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;} 
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+#define NFS(nitro) ios_base::sync_with_stdio(false);cin.tie(0);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+#define all(x) (x).begin(),(x).end()
+#define vec_p vector<pair<int,int>
+#define v vector<int>
+void solve(){
+    int n,m;cin>>n>>m;
+    v adj[n+1];
+    for(int i=0;i<m;i++){
+    int u,x;cin>>u>>x;
+    adj[u].push_back(x);
+    adj[x].push_back(u);
+    }
+}
+int32_t vivek(){
+NFS(NITRO);
+int t;
+cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GraphDS
+
+How do store Graph in memory ? 
+1. Adjacency Matrix: Can be used only for smaller numbers of nodes
+2. Adjacency List: Can be used for larger values of nodes
+
+If the graph is weighted, use pair class to store the weights 
+
